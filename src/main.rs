a69e36d48b143122da621a06c528e941e0db696f
Rory Letteney <rory.letteney@gmail.com>
2022-10-11 16:23:46 -0500

    Added dotenv by default

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 
 mod services;
 use services::{create_user_article, fetch_user_articles, fetch_users};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    
     HttpServer::new(move || {
         App::new()
             .service(fetch_users)
